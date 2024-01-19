ea792b237908d34bf8a030b26d4107a61e17c400
INTERNET69 <126699533+INTERNET69@users.noreply.github.com>
2024-01-19 18:50:07 +0530

    Update DownloadPreferences.kt

--- domain/src/main/java/tachiyomi/domain/download/service/DownloadPreferences.kt
+++ domain/src/main/java/tachiyomi/domain/download/service/DownloadPreferences.kt
@@ -2,14 +2,10 @@ package tachiyomi.domain.download.service
 
 import tachiyomi.core.preference.PreferenceStore
 
-class DownloadPreferences(
-    private val preferenceStore: PreferenceStore,
-) {
+class DownloadPreferences(private val preferenceStore: PreferenceStore) {
 
-    fun downloadOnlyOverWifi() = preferenceStore.getBoolean(
-        "pref_download_only_over_wifi_key",
-        true,
-    )
+    // Use default values directly in the function arguments
+    fun downloadOnlyOverWifi() = preferenceStore.getBoolean("pref_download_only_over_wifi_key", true)
 
     fun saveChaptersAsCBZ() = preferenceStore.getBoolean("save_chapter_as_cbz", true)
 
@@ -19,27 +15,16 @@ class DownloadPreferences(
 
     fun removeAfterReadSlots() = preferenceStore.getInt("remove_after_read_slots", -1)
 
-    fun removeAfterMarkedAsRead() = preferenceStore.getBoolean(
-        "pref_remove_after_marked_as_read_key",
-        false,
-    )
+    // Use default values directly in the function arguments
+    fun removeAfterMarkedAsRead() = preferenceStore.getBoolean("pref_remove_after_marked_as_read_key", false)
 
     fun removeBookmarkedChapters() = preferenceStore.getBoolean("pref_remove_bookmarked", false)
 
-    fun removeExcludeCategories() = preferenceStore.getStringSet(
-        "remove_exclude_categories",
-        emptySet(),
-    )
+    fun removeExcludeCategories() = preferenceStore.getStringSet("remove_exclude_categories", emptySet())
 
     fun downloadNewChapters() = preferenceStore.getBoolean("download_new", false)
 
-    fun downloadNewChapterCategories() = preferenceStore.getStringSet(
-        "download_new_categories",
-        emptySet(),
-    )
+    fun downloadNewChapterCategories() = preferenceStore.getStringSet("download_new_categories", emptySet())
 
-    fun downloadNewChapterCategoriesExclude() = preferenceStore.getStringSet(
-        "download_new_categories_exclude",
-        emptySet(),
-    )
+    fun downloadNewChapterCategoriesExclude() = preferenceStore.getStringSet("download_new_categories_exclude", emptySet())
 }
