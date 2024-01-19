d38c87f4126afb2261b0ce4c94fdd9e01023d1f7
INTERNET69 <126699533+INTERNET69@users.noreply.github.com>
2024-01-19 18:51:37 +0530

    Update Source.kt

--- domain/src/main/java/tachiyomi/domain/source/model/Source.kt
+++ domain/src/main/java/tachiyomi/domain/source/model/Source.kt
@@ -7,19 +7,13 @@ data class Source(
     val supportsLatest: Boolean,
     val isStub: Boolean,
     val pin: Pins = Pins.unpinned,
-    val isUsedLast: Boolean = false,
+    val isUsedLast: Boolean = false
 ) {
 
     val visualName: String
-        get() = when {
-            lang.isEmpty() -> name
-            else -> "$name (${lang.uppercase()})"
-        }
+        get() = if (lang.isEmpty()) name else "$name (${lang.uppercase()})"
 
     val key: () -> String = {
-        when {
-            isUsedLast -> "$id-lastused"
-            else -> "$id"
-        }
+        if (isUsedLast) "$id-lastused" else "$id"
     }
 }
